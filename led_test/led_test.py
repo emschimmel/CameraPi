044e0efe5fd9ca89a814540b721f49b21c550976
emschimmel <marianne@krause.nu>
2017-10-01 21:09:30 +0200

    setup led

--- led_test/led_test.py
+++ led_test/led_test.py
@@ -2,6 +2,7 @@ import ConfigParser
 from time import sleep
 try:
 	import RPi.GPIO as GPIO
+	GPIO.setmode(GPIO.BOARD)                                                                                                    
 except ImportError:
 	print('GPIO unavailable')
  
@@ -10,20 +11,30 @@ class led_test:
 	config.read('config.properties')
 
 	def __init__(self):
-		for lednumer in range(1, 17):
-			self.turn_led_on(lednumer, True)	
+		for lednumber in range(1, 17):
+			self.setup_led(lednumber)	
+
+		for lednumber in range(1, 17):
+			self.turn_led_on(lednumber, True)	
 			sleep(0.25)
 		sleep(5)
-		for lednumer in range(1, 17):
-			self.turn_led_on(lednumer, False)	
+		for lednumber in range(1, 17):
+			self.turn_led_on(lednumber, False)	
 			sleep(0.25)
+
+	def setup_led(self, lednumber):
+		int_led_value = int(self.config.get('Leds', 'led_'+`lednumber`))
+#		try:
+		GPIO.setup(int_led_value, GPIO.OUT)
+#		except:
+#			print('GPIO unavailable, unable to setup led_'+`lednumber`)
 
 
 	def turn_led_on(self, lednumber, status):
 		int_led_value = int(self.config.get('Leds', 'led_'+`lednumber`))
-		try:
-			GPIO.output(int_led_value,status) #turn on the LED
-		except:
-			print('GPIO unavailable, unable to turn led_'+`lednumber`+' to '+`status`)
+#		try:
+		GPIO.output(int_led_value,status) #turn on the LED
+#		except:
+#			print('GPIO unavailable, unable to turn led_'+`lednumber`+' to '+`status`)
 			
 led_test()
